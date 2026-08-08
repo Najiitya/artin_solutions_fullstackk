ee9ecee1815c4e4f3adb16c719c56b02564a7132
Najith Nethwan <nethwannajith@gmail.com>
2026-08-08 14:04:58 +0530

    Implement search functionality in TodoController index method

--- todo-backend/app/Http/Controllers/TodoController.php
+++ todo-backend/app/Http/Controllers/TodoController.php
@@ -8,5 +8,15 @@ use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller
 {
-    
+    public function index(){
+        $query = Todo::where('user_id', Auth::id());
+
+        if ($request->has('search')) {
+            $search = $request->query('search');
+            $query->where(function($q) use ($search) {
+                $q->where('title', 'ilike', "%{$search}%")
+                  ->orWhere('description', 'ilike', "%{$search}%");
+            });
+        }
+    }
 }
