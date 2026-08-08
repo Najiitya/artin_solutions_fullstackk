206385d519845c3e51ded795018b2e89c89b252f
Najith Nethwan <nethwannajith@gmail.com>
2026-08-08 14:12:20 +0530

    Add API resource routes for TodoController

--- todo-backend/routes/api.php
+++ todo-backend/routes/api.php
@@ -3,12 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TodoController;
 
 Route::post('/register',AuthController::class,'register');
 Route::post('/login',AuthController::class,'login');
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout',AuthController::class,'logout');
+
+    Route::apiResource('todos', TodoController::class);
 
     Route::get('/user', function(Request $request){
         return $request->user();
